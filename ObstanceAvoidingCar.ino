da5c9138d909dff8efecefd70eac38a14ad7cadf
Mihir Somani <imihirsomani@gmail.com>
2017-10-22 16:09:44 -0400

    Initial commit

---
+++ ObstanceAvoidingCar.ino
@@ -0,0 +1,151 @@
+#include <Servo.h>
+Servo myservo;
+//Motor A(FL)
+const int motorPin1  = 37;  
+const int motorPin2  = 39;  
+//Motor B(FR)
+const int motorPin3  = 50; 
+const int motorPin4  = 48; 
+//Motor C(BL)
+const int motorPin5  = 11;  
+const int motorPin6  =10;  
+//Motor D(BR)
+const int motorPin7  = 5; 
+const int motorPin8  = 6;
+//Constants for Servo and Ultrasonic
+int pos = 0; 
+const int trigPin = 25;
+const int echoPin = 27;
+const int min = 1;
+const int max = 60;
+long duration;
+int distance;
+int distance1;
+int distance2;
+void setup() {
+  //Sets motor pins as Output
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+  pinMode(motorPin5, OUTPUT);
+  pinMode(motorPin6, OUTPUT);
+  pinMode(motorPin7, OUTPUT);
+  pinMode(motorPin8, OUTPUT);
+  //Setup for Servo and Ultrasonic
+  myservo.attach(45); 
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600);  
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(10);
+  moveForward();
+  distance = calculateDistance();
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(5);
+  if(distance>=min && distance<=max){
+       Stop();
+       //delay(5000);
+       for (pos = 90; pos <= 150; pos += 1) { //Servo turns left
+        myservo.write(pos);              
+        delay(10);                     
+        }
+      delay(1000);
+      distance1 = calculateDistance();
+      Serial.print("Distance: ");
+      Serial.println(distance1);
+      for (pos = 150; pos >= 90; pos -= 1) { 
+        myservo.write(pos);              
+        delay(10);                     
+      }
+      if(distance1>max && distance1!=0){
+          moveLeft();
+          delay(1100);
+        }
+       if(distance1>=min && distance1<=max){
+          for(pos = 90; pos >= 30; pos -=1){
+          myservo.write(pos);              
+          delay(10);  
+        }
+      delay(1000);
+      distance2 = calculateDistance();
+      Serial.print("Distance: ");
+      Serial.println(distance2);
+        for (pos = 30; pos <= 90; pos += 1) { 
+          myservo.write(pos);              
+          delay(10);                     
+        }
+        if(distance2>=min && distance2<=max){
+            moveBackward();
+            delay(2000);
+            moveRight();
+            delay(2200);
+          }
+        if(distance2!= 0 && distance2>max){
+            moveRight();
+            delay(1100);
+          }
+      }
+   }
+}
+void moveForward(){
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    digitalWrite(motorPin5, HIGH);
+    digitalWrite(motorPin6, LOW);
+    digitalWrite(motorPin7, HIGH);
+    digitalWrite(motorPin8, LOW);
+  }
+void moveBackward(){
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
+    digitalWrite(motorPin5, LOW);
+    digitalWrite(motorPin6, HIGH);
+    digitalWrite(motorPin7, LOW);
+    digitalWrite(motorPin8, HIGH);
+  }
+void moveLeft(){
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    digitalWrite(motorPin5, LOW);
+    digitalWrite(motorPin6, HIGH);
+    digitalWrite(motorPin7, HIGH);
+    digitalWrite(motorPin8, LOW);
+  }
+void moveRight(){
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
+    digitalWrite(motorPin5, HIGH);
+    digitalWrite(motorPin6, LOW);
+    digitalWrite(motorPin7, LOW);
+    digitalWrite(motorPin8, HIGH);
+  }
+void Stop(){
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    digitalWrite(motorPin5, LOW);
+    digitalWrite(motorPin6, LOW);
+    digitalWrite(motorPin7, LOW);
+    digitalWrite(motorPin8, LOW);
+  }
+int calculateDistance(){
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;
+  return distance;
+  }
